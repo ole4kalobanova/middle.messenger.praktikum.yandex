10c879fd26aa76b325b763950e2378f4e3a34eab
Olga <ole4kalobanova@icloud.com>
2022-04-03 13:38:50 +0300

    change server

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(__dirname + '/dist'));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(PORT, function () {
