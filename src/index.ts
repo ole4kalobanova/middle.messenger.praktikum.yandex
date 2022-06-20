bc6353b0d3c5945d004165582f06d96876647c2e
Olga <ole4kalobanova@icloud.com>
2022-06-20 13:29:37 +0300

    delete console

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,6 @@ registerComponent(Fields);
 
 document.addEventListener('DOMContentLoaded', () => {
   let renderPage = new Error404({});
-  console.log(window.location, 'window.location')
-  console.log(window.location.pathname, 'window.location.pathname')
   switch (window.location.pathname) {
     case '/':
       renderPage = new LoginPage({});
